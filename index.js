b2d040168461932ee4bae941d06583f0b3909196
Saurabh Pranjale <saurabh.pranjale20@gmail.com>
2024-10-23 17:04:46 +0530

    some changes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function updateCartCount() {
     document.getElementById('cart-count').innerText = cartCount;
 }
 
-
 async function fetchProducts() {
     try {
         const response = await fetch('https://fakestoreapi.com/products');
@@ -18,37 +17,53 @@ async function fetchProducts() {
 
 function displayProducts(products) {
     const productList = document.getElementById('product-list');
-    productList.innerHTML = ''; 
+    productList.innerHTML = '';
 
     products.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('product');
         productDiv.setAttribute('data-id', product.id);
+
+        const isInCart = cart.some(item => item.id === product.id);
+        const buttonText = isInCart ? 'Remove from Cart' : 'Add to Cart';
+
         productDiv.innerHTML = `
-           <section class="d-flex flex-column justify-content-around align-items-center"
-        style="height: max-content;width: 250px;border: 2px solid black;">
-        <h2 class="my-2">${product.title}</h2>
-        <p>Price: $${product.price}</p>
-        <img src="${product.image}" alt="${product.title}" style="height: 180px;" />
-        <button class="add-to-cart my-2">Add to Cart</button>
-    </section>
+            <section class="d-flex flex-column justify-content-around align-items-center"
+            style="height: max-content;width: 250px;border: 2px solid black;">
+                <h2 class="my-2">${product.title}</h2>
+                <p>Price: $${product.price}</p>
+                <img src="${product.image}" alt="${product.title}" style="height: 180px;" />
+                <button class="cart-toggle my-2">${buttonText}</button>
+            </section>
         `;
         productList.appendChild(productDiv);
 
-
-        productDiv.querySelector('.add-to-cart').addEventListener('click', () => {
-            const cartItem = {
-                id: product.id,
-                name: product.title,
-                price: product.price
-            };
-            cart.push(cartItem);
-            localStorage.setItem('cart', JSON.stringify(cart));
+       
+        productDiv.querySelector('.cart-toggle').addEventListener('click', () => {
+         
+            const isInCartNow = cart.some(item => item.id === product.id);
+            if (isInCartNow) {
+         
+                const itemIndex = cart.findIndex(item => item.id === product.id);
+                cart.splice(itemIndex, 1);
+                localStorage.setItem('cart', JSON.stringify(cart));
+                productDiv.querySelector('.cart-toggle').innerText = 'Add to Cart';
+            } else {
+       
+                const cartItem = {
+                    id: product.id,
+                    name: product.title,
+                    price: product.price
+                };
+                cart.push(cartItem);
+                localStorage.setItem('cart', JSON.stringify(cart));
+                productDiv.querySelector('.cart-toggle').innerText = 'Remove from Cart';
+            }
+        
             updateCartCount();
         });
     });
 }
-
 
 window.onload = () => {
     updateCartCount();
